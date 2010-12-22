0790fc87911dfc12e5749befc2affe6e649c949a
Michael Johann <mjohann@rails-experts.com>
2010-12-22 10:30:03 +0100

    After manual merge

--- 7Things/ViewModels/TaskModel.cs
+++ 7Things/ViewModels/TaskModel.cs
@@ -7,187 +7,160 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
+using System.ComponentModel;
+
 namespace _7Things.ViewModels
 {
-  using System;
-  using System.ComponentModel;
-
-  /// <summary>
-  /// The task model.
-  /// </summary>
-  public class TaskModel : INotifyPropertyChanged
-  {
-    #region Constants and Fields
-
     /// <summary>
-    /// The _description.
+    /// The task model.
     /// </summary>
-    private string _description;
-
-    /// <summary>
-    /// The _id.
-    /// </summary>
-    private Guid _id;
-
-    /// <summary>
-    /// The _is done.
-    /// </summary>
-    private bool _isDone;
-
-    /// <summary>
-    /// The _title.
-    /// </summary>
-    private string _title;
-
-    /// <summary>
-    /// The _to be finished.
-    /// </summary>
-    private DateTime _toBeFinished;
-
-    #endregion
-
-    #region Events
-
-    /// <summary>
-    /// The property changed.
-    /// </summary>
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    #endregion
-
-    #region Properties
-
-    /// <summary>
-    /// Gets or sets Description.
-    /// </summary>
-    public string Description
+    public class TaskModel : INotifyPropertyChanged
     {
-      get
-      {
-        return this._description;
-      }
+        #region Constants and Fields
 
-      set
-      {
-        if (value != this._description)
+        /// <summary>
+        /// The _description.
+        /// </summary>
+        private string _description;
+
+        /// <summary>
+        /// The _id.
+        /// </summary>
+        private Guid _id;
+
+        /// <summary>
+        /// The _is done.
+        /// </summary>
+        private bool _isDone;
+
+        /// <summary>
+        /// The _title.
+        /// </summary>
+        private string _title;
+
+        /// <summary>
+        /// The _to be finished.
+        /// </summary>
+        private DateTime _toBeFinished;
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// The property changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets Description.
+        /// </summary>
+        public string Description
         {
-          this._description = value;
-          this.NotifyPropertyChanged("Description");
-        }
-      }
-    }
+            get { return _description; }
 
-    /// <summary>
-    /// Gets or sets Id.
-    /// </summary>
-    public Guid Id
-    {
-      get
-      {
-        return this._id;
-      }
-
-      set
-      {
-        if (value != this._id)
-        {
-          this._id = value;
-          this.NotifyPropertyChanged("Id");
-        }
-      }
-    }
-
-    /// <summary>
-    /// Gets or sets a value indicating whether IsDone.
-    /// </summary>
-    public bool IsDone
-    {
-      get
-      {
-        return this._isDone;
-      }
-
-      set
-      {
-        if (value != this._isDone)
-        {
-          this._isDone = value;
-          this.NotifyPropertyChanged("IsDone");
-        }
-      }
-    }
-
-    /// <summary>
-    /// Gets or sets Title.
-    /// </summary>
-    public string Title
-    {
-      get
-      {
-        return this._title;
-      }
-
-      set
-      {
-        if (value != this._title)
-        {
-          this._title = value;
-          this.NotifyPropertyChanged("Title");
-        }
-      }
-    }
-
-    /// <summary>
-    /// Gets or sets ToBeFinished.
-    /// </summary>
-    public DateTime ToBeFinished
-    {
-      get
-      {
-        return this._toBeFinished;
-      }
-
-      set
-      {
-        if (value != this._toBeFinished)
-        {
-<<<<<<< HEAD
-            get { return _toBeFinished; }
             set
             {
-                if(value != _toBeFinished)
+                if (value != _description)
                 {
-                    _toBeFinished = value;
-                   NotifyPropertyChanged("ToBeFinished");
+                    _description = value;
+                    NotifyPropertyChanged("Description");
                 }
             }
-=======
-          this._toBeFinished = value;
-          this.NotifyPropertyChanged("ToBeFinished");
->>>>>>> e3da5ae79d67f08283c688a31cd8cb3fe316b103
         }
-      }
+
+        /// <summary>
+        /// Gets or sets Id.
+        /// </summary>
+        public Guid Id
+        {
+            get { return _id; }
+
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    NotifyPropertyChanged("Id");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether IsDone.
+        /// </summary>
+        public bool IsDone
+        {
+            get { return _isDone; }
+
+            set
+            {
+                if (value != _isDone)
+                {
+                    _isDone = value;
+                    NotifyPropertyChanged("IsDone");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets Title.
+        /// </summary>
+        public string Title
+        {
+            get { return _title; }
+
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    NotifyPropertyChanged("Title");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets ToBeFinished.
+        /// </summary>
+        public DateTime ToBeFinished
+        {
+            get { return _toBeFinished; }
+
+            set
+            {
+                if (value != _toBeFinished)
+                {
+                    _toBeFinished = value;
+                    NotifyPropertyChanged("ToBeFinished");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// The notify property changed.
+        /// </summary>
+        /// <param name="propertyName">
+        /// The property name.
+        /// </param>
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
     }
-
-    #endregion
-
-    #region Methods
-
-    /// <summary>
-    /// The notify property changed.
-    /// </summary>
-    /// <param name="propertyName">
-    /// The property name.
-    /// </param>
-    private void NotifyPropertyChanged(string propertyName)
-    {
-      PropertyChangedEventHandler handler = this.PropertyChanged;
-      if (null != handler)
-      {
-        handler(this, new PropertyChangedEventArgs(propertyName));
-      }
-    }
-
-    #endregion
-  }
 }
